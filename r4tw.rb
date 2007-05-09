c2d88b222c55aab16fba3599ebc059c9483df062
Simon Baird <simon.baird@gmail.com>
2007-05-09 06:34:09 +0000

    mention BSD license
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/SimonBaird/r4tw/trunk@2190 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- r4tw.rb
+++ r4tw.rb
@@ -2,6 +2,7 @@
 # =r4tw
 # Author:: Simon Baird
 # URL:: http://simonbaird.com/r4tw
+# License:: BSD
 # r4tw is some ruby classes for manipuating TiddlyWikis and tiddlers.
 # It is similar to cook and ginsu but cooler.
 #
