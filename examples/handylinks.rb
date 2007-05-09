4664c8dae2f1e3bb9a77a75d6cf199b85fdf1aa9
Simon Baird <simon.baird@gmail.com>
2007-05-09 06:08:48 +0000

    example tweak
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/SimonBaird/r4tw/trunk@2189 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- examples/handylinks.rb
+++ examples/handylinks.rb
@@ -3,9 +3,8 @@ $LOAD_PATH << '..'
 
 require 'r4tw.rb'
 
-puts "<ul>"
-make_tw { source_url "http://www.tiddlywiki.com/" }.tiddlers_with_tag('contentPublisher').each do |p|
+url = "http://www.tiddlywiki.com/" 
+make_tw { source_url url }.tiddlers_with_tag('contentPublisher').each do |p|
   puts %{<li><a href="#{p.get_slice('URL')}">#{p.name}</a></li>}
 end
-puts "</ul>"
 
