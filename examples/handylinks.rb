c3fff9887f15ef8351c6c2a286314da93e22af6c
Simon Baird <simon.baird@gmail.com>
2007-05-28 16:08:10 +0000

    minor example tweakage
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/SimonBaird/r4tw/trunk@2231 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- examples/handylinks.rb
+++ examples/handylinks.rb
@@ -4,7 +4,7 @@ $LOAD_PATH << '..'
 require 'r4tw.rb'
 
 url = "http://www.tiddlywiki.com/" 
-make_tw { source_url url }.tiddlers_with_tag('contentPublisher').each do |p|
+TiddlyWiki.new.empty_source(url).tiddlers_with_tag('contentPublisher').each do |p|
   puts %{<li><a href="#{p.get_slice('URL')}">#{p.name}</a></li>}
 end
 
--- examples/helloworld.rb
+++ examples/helloworld.rb
@@ -7,13 +7,13 @@ make_tw {
 
   source_file '../tests/empties/2.1.3.html'
 
-  add_tiddler_from_scratch({
+  add_tiddler_from({
     'tiddler'=>'SiteTitle',
     'text'=>'Hello from r4tw',
     'modifier'=>'r4tw',
   })
 
-  add_tiddler_from_file 'hello.js'
+  add_tiddler_from 'hello.js'
 
   to_file 'mytw.html'
 
