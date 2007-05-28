8d228cac4ec371c47af7478ea71d52e4a6e6c3db
Simon Baird <simon.baird@gmail.com>
2007-05-28 16:10:27 +0000

    type in last example update
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/SimonBaird/r4tw/trunk@2232 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- examples/handylinks.rb
+++ examples/handylinks.rb
@@ -4,7 +4,7 @@ $LOAD_PATH << '..'
 require 'r4tw.rb'
 
 url = "http://www.tiddlywiki.com/" 
-TiddlyWiki.new.empty_source(url).tiddlers_with_tag('contentPublisher').each do |p|
+TiddlyWiki.new.source_empty(url).tiddlers_with_tag('contentPublisher').each do |p|
   puts %{<li><a href="#{p.get_slice('URL')}">#{p.name}</a></li>}
 end
 
